479dd965b29570d89dce23c31b05d1ea240f168c
Tara Kreft <tara.kreft@gmail.com>
2015-12-14 12:29:13 -0800

    setting up test directory, package.json and gulpfile update

--- server/gulpfile.js
+++ server/gulpfile.js
@@ -1,18 +1,25 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
 var mocha = require('gulp-mocha');
 
 gulp.task('default', function(){
   gulp.start('mocha/chai', 'jshint');
 });
 
-gulpt.task('jshint', function(){
-  gulp.src(['app/*.js', 'server/*.js', 'test.js'])
+gulp.task('jshint', function(){
+  gulp.src(['/models/*.js', '/test/*.js'])
   .pipe(jshint())
   .pipe(jshint.reporter('default'));
 });
 
+gulp.task('jscs', function (){
+  return gulp.src(['models/*.js', '/*test.js'])
+  .pipe(jscs())
+  .pipe(jscs.reporter());
+})
+
 gulp.task('mocha/chai', function(){
-  return gulp.src('test.js')
+  return gulp.src('/test/*.js')
   .pipe(mocha({reporter:'nyan'}));
 });
