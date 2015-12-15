2ce21930e6bceee6830e65c82229c1589cfe961f
PREET GUJRAL <PREET@PREETGUJRAL.COM>
2015-12-14 17:18:46 -0800

    changed ionic to use sass and added www/lib back to file

---
+++ app/webpack.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+    entry: './www/js/app.js',
+    output: {
+        path: '../server/public',
+        filename: 'bundle.js'
+    },
+    devtool: 'source-map',
+    module: {
+        // preLoaders: [
+            // {
+                // test: /\.js$/, // include .js files
+                // exclude: /node_modules/, // exclude any and all files in the node_modules folder
+                // loader: "jshint-loader"
+            // }
+        // ],
+        loaders: [
+            {
+                test: /\.html$/,
+                loader: 'html'
+            },
+            {
+                test: /\.scss$/,
+                loader: 'style!css?sourceMap!sass?sourceMap'
+            }
+            // {
+                // test: /\.jsx?$/,
+                // exclude: /node_modules/,
+                // loader: 'babel',
+                // query: {
+                    // presets: ['es2015'] //,
+                    // cacheDirectory: true,
+                    // plugins: ['transform-runtime']
+                // }
+            // }
+        ]
+    },
+    sassLoader: {
+        includePaths: ['./scss/content', './scss/core', './scss/includes', './scss/layout', './scss/mobile', './scss/general']
+    }
+};
