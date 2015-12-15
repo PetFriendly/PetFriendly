afca760c9e49fcf7bbf6c241961dc8ff051c575e
dxeware <dxeware@gmail.com>
2015-12-14 21:36:52 -0800

    Changes for Heroku initial deployment..setup either local DB or MongoLab DB uri, read PORT from env (needed for Heroku) or default to 3000, updated README with PF_USE_LOCAL_DB, added Procfile

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # PetFriendly
 Web application to match users with adoptable pets in their area.
+
+#DB setup
+To setup local database for application, set the following environment variable:
+	export PF_USE_LOCAL_DB=1
+Otherwise, the application will use Heroku MongoLab database
--- server/server.js
+++ server/server.js
@@ -11,8 +11,16 @@ app.use(express.static(__dirname + '/'));
 app.use(bodyParser.json());
 app.use(methodOverride());
 
-
-mongoose.connect('mongodb://localhost/pet_app');
+// Set PF_USE_LOCAL_DB=1 for local DB, 
+// otherwise use HEROKU MONGOLAB DB
+if (process.env.PF_USE_LOCAL_DB == true) {
+  mongoose.connect('mongodb://localhost/pet_app');
+  console.log('Connecting to local database...');
+} else {
+  //mongoose.connect(process.env.MONGOLAB_URI);
+  mongoose.connect('mongodb://heroku_vs2nd9mc:73s2p4slp257li6ftg6qh7jjnr@ds029615.mongolab.com:29615/heroku_vs2nd9mc');
+  console.log('Connecting to Heroku MongoLab database...');
+}
 
 /*Pet is going to send to route as /Pets
 http methods are going to save to db collection pets*/
@@ -20,6 +28,6 @@ restify.serve( router, Pet);
 
 app.use(router);
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Express server listening on port 3000')
 });
