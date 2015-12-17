6d98f6268bab6b3965e0731b402555e0031d7db6
Tara Kreft <tara.kreft@gmail.com>
2015-12-16 17:32:59 -0800

    frame of swipe cards

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,6 +4,36 @@ angular.module('starter.controllers', [])
 
 .controller('DetailsCtrl', function($scope) {})
 
+.controller('CardsCtrl', function($scope, $ionicSwipeCardDelegate) {
+  var cardTypes = [
+  { image: './img/cute-dog-2.jpg'},
+  { image: './img/Cute-Dog-1.jpg'},
+  { image: './img/cute-dog-3.jpg'}
+  ];
+
+  $scope.cards = Array.prototype.slice.call(cardTypes, 0, 0);
+
+  $scope.cardSwiped = function(index) {
+    $scope.addCard();
+  };
+
+  $scope.cardDestroyed = function(index) {
+    $scope.cards.splice(index, 1);
+  };
+
+  $scope.addCard = function() {
+    var newCard = cardTypes[Math.floor(Math.random() * cardTypes.length)];
+    newCard.id = Math.random();
+    $scope.cards.push(angular.extend({}, newCard));
+  }
+})
+
+.controller('CardCtrl', function($scope, $ionicSwipeCardDelegate) {
+  $scope.goAway = function() {
+    var card = $ionicSwipeCardDelegate.getSwipeableCard($scope);
+    card.swipe();
+  };
+});
 // .controller('ChatsCtrl', function($scope, Chats) {
 //   // With the new view caching in Ionic, Controllers are only called
 //   // when they are recreated or on app start, instead of every page change.
