3834f8d6d6224c11320c6d0f551058e788567214
dxeware <dxeware@gmail.com>
2015-12-18 15:05:40 -0800

    First pass at saving and displaying favorites . Save working, display only works for 1 pet

--- models/User.js
+++ models/User.js
@@ -13,8 +13,31 @@ var User = new Schema({
   age: String
  },
  petFavs: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Pet'
+    isFav: Boolean,
+    name: String,
+    pfId: String,
+    description: String,
+    age: String, //Not sure if string or number
+    gender: String,
+    size: String,
+    animal: String,
+    contact: {
+      name: String,
+      email: String,
+      phone: String,
+      address1: String,
+      address2: String,
+      city: String,
+      state: String,
+      zip: String,
+    },
+    url: String,
+    photo1: String,
+    photo2: String,
+    photo3: String,
+    options: {
+      specialNeeds: String // add additional as new feature
+    }
   }],
 }, { timestamps: true });
 
