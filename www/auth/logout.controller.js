1136335b898aa8eebfabefbcd83fe5ed3e068755
dxeware <dxeware@gmail.com>
2015-12-19 15:47:36 -0800

    more logout changes

--- www/auth/logout.controller.js
+++ www/auth/logout.controller.js
@@ -1,12 +1,7 @@
-function LogoutCtrl ($scope, $rootScope, $state) {
+function LogoutCtrl ($rootScope, $state) {
   console.log('Entering LogoutCtrl....');
-
-  //$scope.logout = function(){
-    console.log('Logout pressed...');
-    $rootScope.session = undefined;
-    $state.go('login');
-  //};
-
+  $rootScope.session = undefined;
+  $state.go('login');
 }
 
 angular.module('starter.controllers')
